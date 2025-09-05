8ccc64c4ebfe1af725876da296180eefc00448c7
weiweiweiweiweiwiewiwerwerdggw <xianyao.wei.de@gmail.com>
2025-09-05 20:52:19 +0200

    i have navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./Navbar.scss"
-import logo from '/icons/vamonos_pest.jpg'//go up twice
+import logo from '/icons/vamonos_pest.png'//go up twice
 
 const Navbar = () => {
   return (
     <nav>
-      <img src={logo} alt="" />
+      <img src={logo} alt="logo" className='logo' />
       <ul>
         <li>Home</li>
         <li>About us</li>
         <li>Services</li>
-        <li>Contact</li>
+        <li><button className='btn'>Contact Us</button></li>
       </ul>
     </nav>
   )
